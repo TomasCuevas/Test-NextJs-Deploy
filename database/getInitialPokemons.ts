672b391dfc79eebf72919b15b1851a2aeb34aa72
Tomas Cuevas <memo_mister398@hotmail.com>
2022-10-03 22:46:38 -0300

    ssr with hard fetch 50 test

--- database/getInitialPokemons.ts
+++ database/getInitialPokemons.ts
@@ -1,6 +1,6 @@
 import { pokeApi } from "../axios";
 
-export const getInitialPokemons = async (limit = 5) => {
+export const getInitialPokemons = async (limit = 50) => {
   const { data } = await pokeApi.get(`?limit=${limit}`);
   return data;
 };
